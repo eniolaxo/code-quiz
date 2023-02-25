8c7501b2b2f11ddcd1c69cfa6678a0d383b27b6c
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-02-25 20:41:27 +0000

    trying to display the score on the highscore page

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,8 +2,9 @@ var listHighscore = document.querySelector("#highscores");
 
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+//trying to display the score on the highscore page
 for (var i = 0; i<highScores.length; i++){
-  var listHs = document.createElement("li");
+  var listHs = document.createElement("p");
   listHighscore.appendChild(listHs);
   listHs.textContent = highScores[i].initials + " - " + highScores[i].score;
 }
